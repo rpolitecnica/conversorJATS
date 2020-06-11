2d868c5dcbaa193687944bcd6602a5e1bc43b296
luis.yepesm@arus.com.co <luis.yepesm@arus.com.co>
2020-06-11 04:32:04 -0500

    Se cambian las var de conexion a la BD

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,12 @@
 module.exports = {
   database: {
-    host: 'localhost',
-    user: 'root',
-    password: 'Op4qu32020.*',
-    //database: 'bdrevistapoli',
-    database: 'bdrevistapolitecnica'
+    //host: 'localhost',
+    //user: 'root',
+    //password: 'Op4qu32020.*',
+    //database: 'bdrevistapolitecnica'
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'befeb25428cd01',
+    password: 'a5a2be26',
+    database: 'heroku_7c23196717f612e'
   }
 };
